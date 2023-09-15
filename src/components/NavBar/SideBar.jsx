5dbdf0384aceed48b00103771eae1f5ab52e4d62
Suman Saurabh Das <dsumansaurabh@gmail.com>
2023-09-15 11:35:20 +0530

    Added profile page & authentication - Logout feature

--- src/components/NavBar/SideBar.jsx
+++ src/components/NavBar/SideBar.jsx
@@ -14,8 +14,8 @@ function SideBar({ showSidebar, setShowSidebar }) {
   return (
     <div className="text-black dark:text-white">
       {/* Sidebar - START */}
-      <nav className={`${showSidebar ? '' : '-translate-x-full'} sm:translate-x-0 bg-black/[0.5] fixed h-[100vh] overflow-y-auto top-0 w-full sm:w-56 z-40`}>
-        <div className={`${showSidebar ? '' : '-translate-x-56'} sm:translate-x-0 bg-white dark:bg-bgDarkSecondary mt-32 sm:mt-24 w-56 sm:w-full transition-all duration-500`}>
+      <nav className={`${showSidebar ? '' : '-translate-x-full'} sm:translate-x-0 bg-black/[0.5] fixed h-[100vh] top-0 w-full sm:w-56 z-40`}>
+        <div className={`${showSidebar ? '' : '-translate-x-56'} sm:translate-x-0 bg-white dark:bg-bgDarkSecondary overflow-y-auto h-[100vh] mt-32 sm:mt-24 w-56 sm:w-full transition-all duration-500`}>
 
           {/* Categories 1 - START */}
           <div className="flex flex-col">
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,13 +3,36 @@ import { useSelector } from 'react-redux'
 
 function Profile({ showSidebar }) {
   const userInformation = useSelector(state => state.user)
+  const logout = () => {
+    localStorage.clear()
+    window.location.href = '/'
+  }
+  const favoriteMovies = []
+
   return (
     <main className={`main-container 
     ${showSidebar
         ? 'overflow-y-hidden bg-black/[0.5] dark:bg-black/[0.7]'
         : 'overflow-y-scroll'}`}
     >
-      Profile - {userInformation.user.username}
+      <div className="p-4 sm:p-6 lg:p-8">
+        <div className="flex items-center justify-between">
+          <h3 className="font-semibold text-2xl lg:text-3xl">My Profile</h3>
+          <button 
+            className="border-2 border-gray-400 dark:border-white flex lg:font-semibold gap-3 items-center justify-between px-2 lg:px-3 py-1 rounded-md lg:rounded-lg text-sm lg:text-base"
+            onClick={logout}
+          >
+            Logout <i className="uil uil-signout lg:text-xl"></i>
+          </button>
+        </div>
+        <div className="my-8">
+          {
+            !favoriteMovies.length
+              ? <h4 className="">Add favorites or watchlist some movies to see them here !</h4>
+              : <div>Favorite movies</div>
+          }
+        </div>
+      </div>
     </main>
   )
 }
